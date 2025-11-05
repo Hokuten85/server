5bbc14adcd5e1e93697628564781f464ef422dbe
Hokuten85 <kellygrantleanna@gmail.com>
2025-11-04 18:28:03 -0600

    use items75 for AH population

--- START/BROKER.cmd
+++ START/BROKER.cmd
@@ -2,7 +2,7 @@
 echo [%date% %time%] Restarting BROKER...
 
 cd H:\ffxiahbot\bin
-uv run ffxiahbot broker --inp-csv items.csv --buy-items --sell-items
+uv run ffxiahbot broker --inp-csv items75.csv --buy-items --sell-items
 
 echo BROKER was stopped or crashed!
 GOTO onCrash
