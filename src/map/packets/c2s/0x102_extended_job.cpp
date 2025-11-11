1e6b33d0d4264d1eac5b4dd94053a6cfafc1c0fc
Hokuten85 <kellygrantleanna@gmail.com>
2025-11-10 21:12:30 -0600

    Automaton doesn't exist when changing out the head...

--- src/map/packets/c2s/0x102_extended_job.cpp
+++ src/map/packets/c2s/0x102_extended_job.cpp
@@ -221,13 +221,13 @@ void GP_CLI_COMMAND_EXTENDED_JOB::process(MapSession* PSession, CCharEntity* PCh
         {
             if (pupData.Slots[static_cast<uint8_t>(AutomatonSlot::Head)] != 0)
             {
-                PChar->removePetModifiers((CPetEntity*)PChar->PPet);
+                //PChar->removePetModifiers((CPetEntity*)PChar->PPet);
                 puppetutils::setHead(PChar, pupData.Slots[static_cast<uint8_t>(AutomatonSlot::Head)]);
                 petutils::CalculateAutomatonStats(PChar, PChar->PPet);
             }
             else if (pupData.Slots[static_cast<uint8_t>(AutomatonSlot::Frame)] != 0)
             {
-                PChar->removePetModifiers((CPetEntity*)PChar->PPet);
+                //PChar->removePetModifiers((CPetEntity*)PChar->PPet);
                 puppetutils::setFrame(PChar, pupData.Slots[static_cast<uint8_t>(AutomatonSlot::Frame)]);
                 petutils::CalculateAutomatonStats(PChar, PChar->PPet);
             }
