274b726734c89112bf0abf2b5ecc645f2f904933
Hokuten85 <kellygrantleanna@gmail.com>
2025-12-07 17:43:55 -0600

    AF and Relic enhancement 2

--- modules/ivalice_custom/item_mods.sql
+++ modules/ivalice_custom/item_mods.sql
@@ -140,6 +140,34 @@ INSERT INTO item_mods VALUES (15669,12,5); -- Duelist's Tights +1
 UPDATE item_mods SET value = 5 WHERE itemid = 15136 AND modid = 28; -- Duelist's Boots
 UPDATE item_mods SET value = 6 WHERE itemid = 15669 AND modid = 28; -- Duelist's Boots +1
 
+INSERT INTO item_mods VALUES (12514,9,5); -- Rogue's Bonnet
+INSERT INTO item_mods VALUES (15230,298,2); -- Rogue's Bonnet +1
+UPDATE item_mods SET value = 2 WHERE itemid = 12514 AND modid = 298; -- Rogue's Bonnet
+UPDATE item_mods SET value = 8 WHERE itemid = 15230 AND modid = 9; -- Rogue's Bonnet +1
+INSERT INTO item_mods VALUES (12643,9,5); -- Rogue's Vest
+INSERT INTO item_mods VALUES (14478,421,10); -- Rogue's Vest +1
+INSERT INTO item_mods VALUES (12643,9,6); -- Rogue's Vest
+INSERT INTO item_mods VALUES (14478,421,10); -- Rogue's Vest +1
+INSERT INTO item_mods VALUES (13966,11,5); -- Rogue's Armlets
+UPDATE item_mods SET value = 5 WHERE itemid = 13966 and modid = 9; -- Rogue's Armlets
+INSERT INTO item_mods VALUES (13966,520,10); -- Rogue's Armlets
+INSERT INTO item_mods VALUES (14895,11,5); -- Rogue's Armlets +1
+UPDATE item_mods SET value = 5 WHERE itemid = 14895 and modid = 9; -- Rogue's Armlets +1
+UPDATE item_mods SET value = 8 WHERE itemid = 14219 AND modid = 11; -- Rogue's Culottes
+UPDATE item_mods SET value = 8 WHERE itemid = 15566 AND modid IN (9,11); -- Rogue's Culottes +1
+UPDATE item_mods SET value = 60 WHERE itemid = 14094 AND modid = 93; -- Rogue's Poulaines
+UPDATE item_mods SET value = 60 WHERE itemid = 15357 AND modid = 93; -- Rogue's Poulaines +1
+
+UPDATE item_mods SET value = 50 WHERE itemid = 15077 AND modid = 835; -- Assassin's Bonnet
+UPDATE item_mods SET value = 50 WHERE itemid = 15250 AND modid = 835; -- Assassin's Bonnet +1
+UPDATE item_mods SET value = 20 WHERE itemid = 15092 AND modid = 165; -- Assassin's Vest
+UPDATE item_mods SET value = 20 WHERE itemid = 14505 AND modid = 165; -- Assassin's Vest +1
+UPDATE item_mods SET value = 2 WHERE itemid = 14914 AND modid = 303; -- Assassin's Armlets +1
+UPDATE item_mods SET value = 50 WHERE itemid = 15122 AND modid = 897; -- Asn. Culottes
+UPDATE item_mods SET value = 50 WHERE itemid = 15585 AND modid = 897; -- Asn. Culottes +1
+UPDATE item_mods SET value = 5 WHERE itemid = 15137 AND modid = 302; -- Asn. Culottes
+UPDATE item_mods SET value = 5 WHERE itemid = 15670 AND modid = 302; -- Asn. Culottes +1
+
 
 
 
