5f894a2c2a8106c58c057d0f5f92de80f5b1011c
Hokuten85 <kellygrantleanna@gmail.com>
2026-01-16 21:31:07 -0600

    update some mobs to be in different grounds of valor regime

--- scripts/zones/Garlaige_Citadel/mobs/Donjon_Bat.lua
+++ scripts/zones/Garlaige_Citadel/mobs/Donjon_Bat.lua
@@ -6,7 +6,7 @@
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
-    xi.regime.checkRegime(player, mob, 709, 1, xi.regime.type.GROUNDS)
+	xi.regime.checkRegime(player, mob, 710, 1, xi.regime.type.GROUNDS)
 end
 
 return entity
--- scripts/zones/Maze_of_Shakhrami/mobs/Chaser_Bats.lua
+++ scripts/zones/Maze_of_Shakhrami/mobs/Chaser_Bats.lua
@@ -6,7 +6,7 @@
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
-    xi.regime.checkRegime(player, mob, 701, 1, xi.regime.type.GROUNDS)
+	xi.regime.checkRegime(player, mob, 702, 1, xi.regime.type.GROUNDS)
 end
 
 return entity
