b00ed184f2a88fdf6373575912aa3d1d8e16b2cd
Hokuten85 <kellygrantleanna@gmail.com>
2026-01-31 13:58:41 -0600

    update ksnm latent weapons

--- modules/ivalice_custom/item_latents.sql
+++ modules/ivalice_custom/item_latents.sql
@@ -20,3 +20,31 @@ REPLACE INTO item_latents
 SELECT itemid, modid, value, 62, latentParam
 FROM item_latents
 WHERE latentid = 8; -- Latents based on Subjob now work for main job
+
+REPLACE INTO `item_latents` VALUES (17207,141, 6,47,0); -- expunger
+REPLACE INTO `item_latents` VALUES (17207,973, 6,47,0); -- expunger
+REPLACE INTO `item_latents` VALUES (17275,141, 6,47,0); -- coffinmaker
+REPLACE INTO `item_latents` VALUES (17275,973, 6,47,0); -- coffinmaker
+REPLACE INTO `item_latents` VALUES (17451,287,23,47,0); -- morgenstern -- 50
+REPLACE INTO `item_latents` VALUES (17451,973, 6,47,0); -- morgenstern
+REPLACE INTO `item_latents` VALUES (17509,287,15,47,0); -- destroyers -- 20
+REPLACE INTO `item_latents` VALUES (17509,973, 6,47,0); -- destroyers
+REPLACE INTO `item_latents` VALUES (17589,287,18,47,0); -- thyrsusstab -- 70
+REPLACE INTO `item_latents` VALUES (17589,973, 6,47,0); -- thyrsusstab
+REPLACE INTO `item_latents` VALUES (17699,287,25,47,0); -- dissector -- 48
+REPLACE INTO `item_latents` VALUES (17699,973, 6,47,0); -- dissector
+REPLACE INTO `item_latents` VALUES (17793,287,15,47,0); -- senjuinrikio -- 40
+REPLACE INTO `item_latents` VALUES (17793,973, 6,47,0); -- senjuinrikio
+REPLACE INTO `item_latents` VALUES (17827,287,18,47,0); -- michishiba-no-tsuyu -- 83
+REPLACE INTO `item_latents` VALUES (17827,973, 6,47,0); -- michishiba-no-tsuyu
+REPLACE INTO `item_latents` VALUES (17944,287,17,47,0); -- retributor -- 50
+REPLACE INTO `item_latents` VALUES (17944,973, 6,47,0); -- retributor
+REPLACE INTO `item_latents` VALUES (18005,287,20,47,0); -- heart_snatcher -- 35
+REPLACE INTO `item_latents` VALUES (18005,973, 6,47,0); -- heart_snatcher
+REPLACE INTO `item_latents` VALUES (18053,287,21,47,0); -- gravedigger -- 95
+REPLACE INTO `item_latents` VALUES (18053,973, 6,47,0); -- gravedigger
+REPLACE INTO `item_latents` VALUES (18097,287,23,47,0); -- gondo-shizunori -- 95
+REPLACE INTO `item_latents` VALUES (18097,973, 6,47,0); -- gondo-shizunori
+REPLACE INTO `item_latents` VALUES (18217,287,20,47,0); -- rampager -- 94
+REPLACE INTO `item_latents` VALUES (18217,973, 6,47,0); -- rampager
+REPLACE INTO `item_latents` VALUES (18378,973, 6,47,0); -- subduer
---
+++ modules/ivalice_custom/item_weapon.sql
@@ -0,0 +1,1 @@
+UPDATE item_weapon SET delay = 170 WHERE itemid = 18005; -- heart_snatcher
